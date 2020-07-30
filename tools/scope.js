b15873e3d51296f8dfa55db6a5d3e5ad02cbc1f5
qianjun <qianjun@gaoding.com>
2020-07-30 15:17:15 +0800

    add build script

---
+++ tools/scope.js
@@ -0,0 +1,6 @@
+var webAnimationsSourceTarget = 'web-animations';
+var WEB_ANIMATIONS_TESTING = false;
+
+window.webAnimationsShared = {};
+window.webAnimations1 = {};
+window.webAnimationsNext = {};
