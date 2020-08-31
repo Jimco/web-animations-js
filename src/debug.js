e18aed563fb38b6e9c48707e15ae986076831715
qianjun <qianjun@gaoding.com>
2020-08-31 18:27:13 +0800

    fix: 增加 Web Animations API 原生支持判断

---
+++ src/debug.js
@@ -0,0 +1,5 @@
+window.webAnimationsShared = {};
+window.webAnimations1 = {};
+window.webAnimationsNext = {};
+window.WEB_ANIMATIONS_TESTING = false;
+window.webAnimationsTesting = null;
--- src/element-animatable.js
+++ src/element-animatable.js
@@ -13,11 +13,56 @@
 // limitations under the License.
 
 (function(scope) {
-  window.Element.prototype.animate = function(effectInput, options) {
-    var id = '';
-    if (options && options.id) {
-      id = options.id;
+  function isPropertySupported(property) {
+    if (property in document.body.style) return true;
+    var prefixes = ['Moz', 'Webkit', 'O', 'ms', 'Khtml'];
+    var prefProperty = property.charAt(0).toUpperCase() + property.substr(1);
+
+    for (var i = 0; i < prefixes.length; i++) {
+      if ((prefixes[i] + prefProperty) in document.body.style) return true;
     }
-    return scope.timeline._play(scope.KeyframeEffect(this, effectInput, options, id));
+
+    return false;
+  }
+
+  function isWebAnimationsApiSupported() {
+    if (document.documentElement.animate) {
+      var player = document.documentElement.animate([], 0);
+
+      if (!player) return false;
+
+      var animProperty = [
+        'play', 'currentTime', 'pause', 'reverse',
+        'playbackRate', 'cancel', 'finish', 'startTime', 'playState',
+      ];
+
+      for (var i = 0, l = animProperty.length; i < l; i++) {
+        if (player[animProperty[i] === undefined]) return false;
+      }
+
+      return true;
+    }
+  }
+
+  var isAnimationsApiSupported = isWebAnimationsApiSupported();
+  var originalElementAnimate = window.Element.prototype.animate;
+  // var isOffsetPathSupported = isPropertySupported('offset-path');
+  var isOffsetPathSupported = false;
+
+  window.Element.prototype.animate = function(effectInput, options) {
+    var hasOffsetPathProperty = ~JSON.stringify(effectInput).indexOf('offsetPath')
+
+    if (
+        !isAnimationsApiSupported
+          || (!isOffsetPathSupported && hasOffsetPathProperty)
+      ) {
+      var id = '';
+      if (options && options.id) {
+        id = options.id;
+      }
+      return scope.timeline._play(scope.KeyframeEffect(this, effectInput, options, id));
+    }
+
+    return originalElementAnimate.call(this, effectInput, options);
   };
 })(webAnimations1);
